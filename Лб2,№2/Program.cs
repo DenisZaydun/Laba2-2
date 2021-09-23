9551e9076069d5c494270f5805f3e035a8abdf33
Денис Зайдун <denzaydunden@gmail.com>
2021-09-23 16:44:22 +0300

    first

---
+++ Лб2,№2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Лб2__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.Write("Введіть першу сторону прямокутника: ");
+            double side1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Введіть другу сторону прямокутника: ");
+            double side2 = double.Parse(Console.ReadLine());
+
+            Rectangle firstRectangle = new Rectangle(side1, side2);
+
+            firstRectangle.GetInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Лб2,№2/Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Лб2__2
+{
+    class Rectangle
+    {
+        private double _side1, _side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            _side1 = side1;
+            _side2 = side2;
+        }
+
+        private double AreaCalculator()
+        {
+            double area = _side1 * _side2;
+            return area;
+        }
+
+
+        private double PerimeterCalculator()
+        {
+            double perimeter = (_side1 * 2) + (_side2 * 2);
+            return perimeter;
+        }
+
+        public double Area  //Создать два свойства double Area и double Perimeter с одним методом доступа get.
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Периметр прямокутника: {Perimeter}");
+            Console.WriteLine($"Площа прямокутника: {Area}");
+        }
+    }
+}
